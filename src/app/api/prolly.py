6c07f0b2599121b6b4251c95feb8d5b284a21c27
William Phan <willdphan@gmail.com>
2024-10-26 10:52:40 -0700

    cors

--- src/app/api/prolly.py
+++ src/app/api/prolly.py
@@ -238,7 +238,7 @@ def fastapi_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://localhost:3001"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
